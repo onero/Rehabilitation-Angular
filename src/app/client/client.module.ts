980b554b97ad05fb92087143cf6ce6d554f93ece
Sk0vgaard <Sk0vgaard@live.dk>
2018-04-23 13:06:10 +0200

    #10672 Information Component
    Added information component and added the html to exercise HTML

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExercisesComponent } from './exercises/exercises.component';
+import { ExerciseInformationComponent } from './exercise-information/exercise-information.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ExercisesComponent]
+  declarations: [ExercisesComponent, ExerciseInformationComponent]
 })
 export class ClientModule { }
---
+++ src/app/client/exercise-information/exercise-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rehab-exercise-information',
+  templateUrl: './exercise-information.component.html',
+  styleUrls: ['./exercise-information.component.scss']
+})
+export class ExerciseInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
