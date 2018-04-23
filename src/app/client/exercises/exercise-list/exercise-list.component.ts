a2fd87693d61b59a0572bb53877e942357605d0c
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2018-04-23 14:42:26 +0200

    #10673 Added List
    Added list in html and bound it to local list in controller.

--- src/app/client/exercises/exercise-list/exercise-list.component.ts
+++ src/app/client/exercises/exercise-list/exercise-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ExerciseModel} from '../../shared/exercise.model';
 
 @Component({
   selector: 'rehab-exercise-list',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciseListComponent implements OnInit {
 
-  constructor() { }
+  exercisesFromClient: ExerciseModel[];
+
+  constructor() {
+  }
+
+  private fillListWithMock() {
+    for (let i = 0; i <= 4; i++) {
+      this.exercisesFromClient.push(
+        {
+          title: `MockClient${i}`,
+          repetition: `${i} x ${i}`,
+          category: `MockCategory${i}`,
+          description: `Mock Description ${i}`
+        }
+      );
+    }
+    this.exercisesFromClient.push();
+  }
 
   ngOnInit() {
+    this.exercisesFromClient = [];
+    this.fillListWithMock();
   }
 
 }
