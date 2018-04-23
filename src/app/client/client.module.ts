f4cc010530310087f621ce295d04488e8f18c3f3
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2018-04-23 13:08:53 +0200

    #10673 Added Component
    Added ExerciseList Component

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExercisesComponent } from './exercises/exercises.component';
+import { ExerciseListComponent } from './exercises/exercise-list/exercise-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ExercisesComponent]
+  declarations: [ExercisesComponent, ExerciseListComponent]
 })
 export class ClientModule { }
