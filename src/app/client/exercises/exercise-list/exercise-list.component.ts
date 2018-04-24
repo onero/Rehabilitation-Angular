fc89ba9c69588716f5e90cc7987535c8feee2b3e
Sk0vgaard <Sk0vgaard@live.dk>
2018-04-24 14:03:28 +0200

    E3#10692 - Eventemitter for list

--- src/app/client/exercises/exercise-list/exercise-list.component.ts
+++ src/app/client/exercises/exercise-list/exercise-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import {ExerciseModel} from '../../shared/exercise.model';
 import {ExerciseService} from '../shared/exercise.service';
-import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'rehab-exercise-list',
@@ -9,6 +8,9 @@ import {forEach} from '@angular/router/src/utils/collection';
   styleUrls: ['./exercise-list.component.scss']
 })
 export class ExerciseListComponent implements OnInit {
+
+  @Output()
+  exerciseSelected = new EventEmitter<ExerciseModel>();
 
   exercisesFromClient: ExerciseModel[];
 
@@ -40,6 +42,6 @@ export class ExerciseListComponent implements OnInit {
   }
 
   onExerciseClick(exercise: ExerciseModel) {
-    // TODO MSP Dependency inject this exercise into videocomponent.
+    this.exerciseSelected.emit(exercise);
   }
 }
