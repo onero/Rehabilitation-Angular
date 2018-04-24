b6b703bab36b8aacce3d19cfcc81ae73b8bad7d1
Adam Hansen <adamlars90@gmail.com>
2018-04-24 14:19:56 +0200

    #10765-Implemented async read

---
+++ src/app/therapist/manage-exercises/manage-exercises-list/manage-exercises-list.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ExerciseModel} from '../../../client/shared/exercise.model';
+import {ExerciseService} from '../../../client/exercises/shared/exercise.service';
+
+@Component({
+  selector: 'rehab-manage-exercises-list',
+  templateUrl: './manage-exercises-list.component.html',
+  styleUrls: ['./manage-exercises-list.component.scss']
+})
+export class ManageExercisesListComponent implements OnInit {
+
+  $exercises: Observable<ExerciseModel[]>;
+
+  constructor(private exerciseService: ExerciseService) {
+    this.$exercises = exerciseService.getExercises();
+  }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/therapist/therapist.module.ts
+++ src/app/therapist/therapist.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManageExercisesComponent } from './manage-exercises/manage-exercises.component';
+import { ManageExercisesListComponent } from './manage-exercises/manage-exercises-list/manage-exercises-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ManageExercisesComponent]
+  declarations: [ManageExercisesComponent, ManageExercisesListComponent]
 })
 export class TherapistModule { }
