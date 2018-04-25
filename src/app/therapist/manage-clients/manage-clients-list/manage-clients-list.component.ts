162f2ed89e406d00eb3a6fa4a30d825e33c8a00a
Sk0vgaard <Sk0vgaard@live.dk>
2018-04-25 16:01:51 +0200

    #10993 Mocking the list
    Can now mock the list with one client

--- src/app/therapist/manage-clients/manage-clients-list/manage-clients-list.component.ts
+++ src/app/therapist/manage-clients/manage-clients-list/manage-clients-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientModel } from '../../shared/client.model';
 
 @Component({
   selector: 'rehab-manage-clients-list',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageClientsListComponent implements OnInit {
 
+  clientsModel: ClientModel;
+
   constructor() { }
 
   ngOnInit() {
+    this.mockClient1();
+    this.mockClient2();
+  }
+
+  private mockClient1() {
+    this.clientsModel = {
+      fullName: 'Mathias Skovgaard',
+      address: 'Amagervej 14, 6705 Esbjerg Ø',
+      phone: '42752316',
+      email: 'MathiasSkovgaardRasmussen@gmail.com'
+    };
+  }
+
+  private mockClient2() {
+    this.clientsModel = {
+      fullName: 'Mathias Plougmann',
+      address: 'Intetsted 15',
+      phone: '1234',
+      email: 'HrJensen@gmail.com'
+    };
   }
 
   addClient() {
