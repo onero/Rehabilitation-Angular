d2fa94c909ddfdca8f6d2190048835df26dcbacd
Sk0vgaard <Sk0vgaard@live.dk>
2018-04-30 10:13:48 +0200

    E0 #11104 - Login View
    LoginView with some functionality
    CSS aswell

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import {AngularFireAuthModule} from 'angularfire2/auth';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from './shared/login.service';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    FormsModule,
+    HttpModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent],
+  providers: [LoginService]
 })
 export class AuthModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginEntity } from '../shared/login.entity';
 
 @Component({
   selector: 'rehab-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  user = new LoginEntity('', '');
+  submitted = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() { this.submitted = true; }
 }
