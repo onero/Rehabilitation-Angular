d90d8ac5c9da8f1215ddb6f16a6ab9ba3da79a3c
Adam Hansen <adamlars90@gmail.com>
2018-04-30 10:01:26 +0200

    #11111 Implemented design

---
+++ src/app/therapist/manage-clients/manage-clients-detail/assign-exercise/assign-exercise.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {ExerciseModel} from '../../../../client/shared/exercise.model';
+
+@Component({
+  selector: 'rehab-assign-exercise',
+  templateUrl: './assign-exercise.component.html',
+  styleUrls: ['./assign-exercise.component.scss']
+})
+export class AssignExerciseComponent implements OnInit {
+
+  $exercises: Observable<ExerciseModel>;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
