6a795edb126960df039befc23039cde9ded28d8f
Sk0vgaard <Sk0vgaard@live.dk>
2018-05-07 14:55:39 +0200

    #5986 onDelete
    Now delete the client in the collection with function call.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,8 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+const CLIENTS_COLLECTION = 'Clients';
 exports.onDeleteUser = functions.auth.user().onDelete(event => {
     const uid = event.uid;
-    console.log(uid);
+    const clientRef = admin.firestore().doc(`${CLIENTS_COLLECTION}/${uid}`);
+    clientRef.get().then(doc => {
+        if (doc.exists) {
+            clientRef.delete().then(() => {
+                console.log('DELETED USER ID: ', uid);
+            });
+        }
+        else {
+            console.log('COULD NOT DELETED USER ID: ', uid);
+        }
+    });
 });
 //# sourceMappingURL=index.js.map
