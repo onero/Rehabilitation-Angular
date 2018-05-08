7681f883409fa652ffb3e69fdf821f1a2dc43eb6
Sk0vgaard <Sk0vgaard@live.dk>
2018-05-08 10:46:55 +0200

    #5638 floppy save

--- src/app/therapist/manage-clients/client-evaluations/milestone-detail/milestone-detail.component.ts
+++ src/app/therapist/manage-clients/client-evaluations/milestone-detail/milestone-detail.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {VisitEntity} from '../../../../shared/entities/visit.entity';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'rehab-milestone-detail',
@@ -18,9 +19,30 @@ export class MilestoneDetailComponent implements OnInit {
   @Input()
   currentVisit: VisitEntity;
 
+  @Output()
+  updateEvaluation = new EventEmitter<string>();
+
+  allowEdit = !environment.clientMode;
+
+  editMode = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * Change save icon to edit icon and so on.
+   */
+  toggleEvaluationEdit() {
+    this.editMode = !this.editMode;
+  }
+
+  /**
+   * Tells when the update is called.
+   */
+  onEvaluationUpdated() {
+    this.updateEvaluation.emit(this.title);
+    this.editMode = false;
+  }
 }
