c9b93c48723c5438620054a69e5b306b2ded628f
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2018-05-23 12:49:04 +0200

    Added button to Add Exercise
    Button routes to ManageExercise

--- src/app/therapist/manage-clients/manage-clients-detail/assign-exercise/assign-exercise.component.ts
+++ src/app/therapist/manage-clients/manage-clients-detail/assign-exercise/assign-exercise.component.ts
@@ -3,13 +3,17 @@ import {Observable} from 'rxjs/Observable';
 import {ExerciseModel} from '../../../../client/shared/exercise.model';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {RehabModalService} from '../../../../shared/services/rehab-modal.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'rehab-assign-exercise',
   templateUrl: './assign-exercise.component.html',
   styleUrls: ['./assign-exercise.component.scss']
 })
+
 export class AssignExerciseComponent implements OnInit {
+
+  private MANAGE_EXERCISES_ROUTE = 'therapist/exercises';
 
   @Input()
   exercises: ExerciseModel[] = [];
@@ -20,7 +24,8 @@ export class AssignExerciseComponent implements OnInit {
   @Output()
   exerciseAssigned = new EventEmitter<string>();
 
-  constructor(public modalService: RehabModalService) {
+  constructor(public modalService: RehabModalService,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -41,5 +46,9 @@ export class AssignExerciseComponent implements OnInit {
   assignSelectedExercise(exercise: ExerciseModel) {
     this.exercises.push(exercise);
     this.exerciseAssigned.emit(exercise.uid);
+  }
+
+  routeToExercise() {
+    this.router.navigateByUrl(this.MANAGE_EXERCISES_ROUTE);
   }
 }
